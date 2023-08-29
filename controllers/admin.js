f935dd5bfbd4ef703de6fe79d4f2f8c9ba01481c
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-08-29 13:14:30 +0530

    many to many relations

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
+  Product.create({
     title: title,
     price: price,
     description: description,
-    imageUrl: imageUrl
+    imageUrl: imageUrl,
+    userId: req.user.id
   }).then(() => {
     res.redirect('/admin/products');
   }).catch(err => console.log(err))
@@ -29,15 +30,18 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findAll({ where: { id: prodId } }).then(([product]) => {
-    if (!product) res.redirect('/');
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  }).catch(err => console.log(err))
+  req.user.getProducts({ where: { id: prodId } })
+    // Product.findAll({ where: { id: prodId } })
+    .then((products) => {
+      const product = products[0]
+      if (!product) res.redirect('/');
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    }).catch(err => console.log(err))
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -59,13 +63,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((products) => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  })
+  // Product.findAll()
+  req.user.getProducts()
+    .then((products) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
 };
 
 exports.postDeleteProduct = (req, res, next) => {
